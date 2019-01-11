1c2e9680aec5a405f3297760b547f21bb23524c9
Germán Larraín <glarrain@users.noreply.github.com>
2019-01-11 17:39:00 -0300

    WIP fix files left open

--- tests/test_rcv.py
+++ tests/test_rcv.py
@@ -17,82 +17,63 @@ class ProcessRcvCsvFileTests(TestCase):
     def test_ok(self) -> None:
         total_rows = 0
 
-        file_csv = open('tests/test_data/rcv_csv/purchase_all_document_types.csv', mode='r')
-        # TODO: fix type
-        rows_generator = rcv.process_rcv_csv_file(  # type: ignore
-            file_csv,
-            self.owner_rut.canonical)
-        for row in rows_generator:
-            self.assertIn('receptor_rut', row)
-            self.assertIsInstance(row['receptor_rut'], rut.Rut)
-            self.assertTrue(self.owner_rut == row['receptor_rut'])
+        with open('tests/test_data/rcv_csv/purchase_all_document_types.csv', mode='r') as f:
+            # TODO: fix type
+            rows_generator = rcv.process_rcv_csv_file(  # type: ignore
+                f, self.owner_rut.canonical)
+            for row in rows_generator:
+                self.assertIn('receptor_rut', row)
+                self.assertIsInstance(row['receptor_rut'], rut.Rut)
+                self.assertTrue(self.owner_rut == row['receptor_rut'])
 
-            self.assertIn('emisor_rut', row)
-            self.assertIsInstance(row['emisor_rut'], rut.Rut)
+                self.assertIn('emisor_rut', row)
+                self.assertIsInstance(row['emisor_rut'], rut.Rut)
 
-            self.assertIn('tipo_dte', row)
-            self.assertIsInstance(row['tipo_dte'], int)
+                self.assertIn('tipo_dte', row)
+                self.assertIsInstance(row['tipo_dte'], int)
 
-            self.assertIn('folio', row)
-            self.assertIsInstance(row['folio'], int)
+                self.assertIn('folio', row)
+                self.assertIsInstance(row['folio'], int)
 
-            self.assertIn('monto_total', row)
-            self.assertIsInstance(row['monto_total'], int)
+                self.assertIn('monto_total', row)
+                self.assertIsInstance(row['monto_total'], int)
 
-            self.assertIn('fecha_recepcion_datetime', row)
-            self.assertIsInstance(row['fecha_recepcion_datetime'], datetime.datetime)
+                self.assertIn('fecha_recepcion_datetime', row)
+                self.assertIsInstance(row['fecha_recepcion_datetime'], datetime.datetime)
 
-            self.assertIn('fecha_emision_date', row)
-            self.assertIsInstance(row['fecha_emision_date'], datetime.date)
+                self.assertIn('fecha_emision_date', row)
+                self.assertIsInstance(row['fecha_emision_date'], datetime.date)
 
-            total_rows = total_rows + 1
+                total_rows = total_rows + 1
         self.assertEqual(total_rows, self.total_rows)
 
     def test_max_data_row(self) -> None:
-        file_csv = open('tests/test_data/rcv_csv/purchase_all_document_types.csv', mode='r')
-        # TODO: fix type
-        rows_generator = rcv.process_rcv_csv_file(  # type: ignore
-            file_csv,
-            self.owner_rut.canonical,
-            max_data_rows=1)
+        with open('tests/test_data/rcv_csv/purchase_all_document_types.csv', mode='r') as f:
+            # TODO: fix type
+            rows_generator = rcv.process_rcv_csv_file(  # type: ignore
+                f, self.owner_rut.canonical, max_data_rows=1)
 
-        with self.assertRaisesRegex(Exception, r'Exceeded \'max_data_rows\' value: 1\.'):
-            for _ in rows_generator:
-                continue
+            with self.assertRaisesRegex(Exception, r'Exceeded \'max_data_rows\' value: 1\.'):
+                for _ in rows_generator:
+                    continue
 
     def test_invalid_row(self) -> None:
-        file_csv = open('tests/test_data/rcv_csv/purchase_invalid_row.csv', mode='r')
-        # TODO: fix type
-        rows_generator = rcv.process_rcv_csv_file(  # type: ignore
-            file_csv,
-            self.owner_rut.canonical)
-
         excepted_message = (
             r"Error deserializing row 1 of CSV file: "
             r"\('Validation errors during deserialization.', "
             r"{'Fecha Docto': \['Not a valid date\.'\]}\)"
         )
-        with self.assertRaisesRegex(Exception, excepted_message):
-            for _ in rows_generator:
-                continue
+
+        with open('tests/test_data/rcv_csv/purchase_invalid_row.csv', mode='r') as f:
+            # TODO: fix type
+            rows_generator = rcv.process_rcv_csv_file(  # type: ignore
+                f, self.owner_rut.canonical)
+
+            with self.assertRaisesRegex(Exception, excepted_message):
+                for _ in rows_generator:
+                    continue
 
     def test_missing_row(self) -> None:
-        file_csv = open('tests/test_data/rcv_csv/purchase_missing_row.csv', mode='r')
-        # TODO: fix type
-        rows_generator = rcv.process_rcv_csv_file(  # type: ignore
-            file_csv,
-            self.owner_rut.canonical)
-
-        with self.assertRaises(Exception) as context_manager:
-            for _ in rows_generator:
-                continue
-
-        exception = context_manager.exception
-        self.assertEqual(len(exception.args), 2)
-        message, fields_name = exception.args
-        self.assertEqual(
-            message,
-            'CSV file field names do not match those expected, or their order.')
         expected_fields_name = [
             'Tipo Doc', 'Tipo Compra', 'RUT Proveedor', 'Razon Social', 'Folio', 'Fecha Docto',
             'Fecha Recepcion', 'Fecha Acuse', 'Monto Exento', 'Monto Neto', 'Monto IVA Recuperable',
@@ -102,18 +83,33 @@ class ProcessRcvCsvFileTests(TestCase):
             'Tabacos Cigarrillos', 'Tabacos Elaborados', 'NCE o NDE sobre Fact. de Compra',
             'Codigo Otro Impuesto', 'Valor Otro Impuesto', 'Tasa Otro Impuesto'
         ]
+
+        with open('tests/test_data/rcv_csv/purchase_missing_row.csv', mode='r') as f:
+            # TODO: fix type
+            rows_generator = rcv.process_rcv_csv_file(  # type: ignore
+                f, self.owner_rut.canonical)
+
+            with self.assertRaises(Exception) as context_manager:
+                for _ in rows_generator:
+                    continue
+
+        exception = context_manager.exception
+        self.assertEqual(len(exception.args), 2)
+        message, fields_name = exception.args
+        self.assertEqual(
+            message,
+            'CSV file field names do not match those expected, or their order.')
         self.assertListEqual(fields_name, expected_fields_name)
 
     def test_invalid_csv(self) -> None:
-        file_csv = open('tests/test_data/rcv_csv/invalid.csv', mode='r')
-        # TODO: fix type
-        rows_generator = rcv.process_rcv_csv_file(  # type: ignore
-            file_csv,
-            self.owner_rut.canonical)
+        with open('tests/test_data/rcv_csv/invalid.csv', mode='r') as f:
+            # TODO: fix type
+            rows_generator = rcv.process_rcv_csv_file(  # type: ignore
+                f, self.owner_rut.canonical)
 
-        with self.assertRaises(Exception) as context_manager:
-            for _ in rows_generator:
-                continue
+            with self.assertRaises(Exception) as context_manager:
+                for _ in rows_generator:
+                    continue
 
         exception = context_manager.exception
         self.assertEqual(len(exception.args), 2)
