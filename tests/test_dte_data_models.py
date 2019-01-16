136eb7abfb984e09a98b1e83c6ce535f086cc767
Germán Larraín <glarrain@users.noreply.github.com>
2019-01-15 21:25:45 -0300

    dte: add module `data_models`
    Include the following 'dataclasses' (and some related validators):
    - `DteNaturalKey`
    - `DteDataL0`
    - `DteDataL1`
    - `DteDataL2`
    
    Tests have not been implemented.

---
+++ tests/test_dte_data_models.py
@@ -0,0 +1,48 @@
+import unittest
+
+from cl_sii.rut import Rut  # noqa: F401
+
+from cl_sii.dte.constants import TipoDteEnum  # noqa: F401
+from cl_sii.dte.data_models import (  # noqa: F401
+    DteDataL0, DteDataL1, DteDataL2, DteNaturalKey,
+    validate_contribuyente_razon_social, validate_dte_folio, validate_dte_monto_total,
+)
+
+
+class DteNaturalKeyTest(unittest.TestCase):
+
+    # TODO: implement!
+    pass
+
+
+class DteDataL0Test(unittest.TestCase):
+
+    # TODO: implement!
+    pass
+
+
+class DteDataL1Test(unittest.TestCase):
+
+    # TODO: implement!
+    pass
+
+
+class DteDataL2Test(unittest.TestCase):
+
+    # TODO: implement!
+    pass
+
+
+class FunctionsTest(unittest.TestCase):
+
+    def test_validate_contribuyente_razon_social(self) -> None:
+        # TODO: implement!
+        pass
+
+    def test_validate_dte_folio(self) -> None:
+        # TODO: implement!
+        pass
+
+    def test_validate_dte_monto_total(self) -> None:
+        # TODO: implement!
+        pass
