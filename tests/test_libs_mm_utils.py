e5f3fd658a6f6575215e27ac871378d59c9b3695
Germán Larraín <glarrain@users.noreply.github.com>
2019-04-04 17:43:38 -0300

    libs: add module `mm_utils`
    Custom marshmallow field `CustomMarshmallowDateField`.
    
    Test have not been implemented.

---
+++ tests/test_libs_mm_utils.py
@@ -0,0 +1,10 @@
+import unittest
+
+from cl_sii.libs.mm_utils import CustomMarshmallowDateField  # noqa: F401
+
+
+class CustomMarshmallowDateFieldTest(unittest.TestCase):
+
+    def test_x(self) -> None:
+        # TODO: implement!
+        pass
