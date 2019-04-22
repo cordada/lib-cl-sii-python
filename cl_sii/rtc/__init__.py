4b3ee0bcebca948573313ba1a4f1d4f6432486b1
Germán Larraín <glarrain@users.noreply.github.com>
2019-04-22 15:50:11 -0400

    add sub-package `rtc`
    For SII's "RTC/RPETC". Concepts and acronyms used interchangeably:
    - "Registro Transferencia de Crédito" (RTC)
    - "Registro Público Electrónico de Transferencia de Crédito" (RPETC)
    - "Registro Electrónico de Cesión de Créditos"

---
+++ cl_sii/rtc/__init__.py
@@ -0,0 +1,11 @@
+"""
+SII RTC/RPETC
+=============
+
+Concepts and acronyms used interchangeably:
+
+* "Registro Transferencia de Crédito" (RTC)
+* "Registro Público Electrónico de Transferencia de Crédito" (RPETC)
+* "Registro Electrónico de Cesión de Créditos"
+
+"""
