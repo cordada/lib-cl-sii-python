0984b00e8a0a957569c364716af74c2ee271e614
Germán Larraín <glarrain@users.noreply.github.com>
2019-05-04 15:37:43 -0400

    scripts: improve 'example.py'
    Based on (almost identical to)
    https://github.com/fynpal/lib-cl-sii-api-python/blob/74e19d9/scripts/example.py

--- scripts/example.py
+++ scripts/example.py
@@ -3,14 +3,20 @@
 Example script
 ==============
 
+Does X and Y, and then Z.
 
-Example::
+Example
+-------
+
+For example, to do X, run::
 
     ./scripts/example.py arg1 arg2 arg3
 
 """
+import logging
 import os
 import sys
+from datetime import datetime
 from typing import Sequence
 
 try:
@@ -22,9 +28,53 @@ except ImportError:
     import cl_sii  # noqa: F401
 
 
+logger = logging.getLogger(__name__)
+root_logger = logging.getLogger()
+
+
+###############################################################################
+# logging config
+###############################################################################
+
+_loggers = [logger, logging.getLogger('cl_sii')]
+for _logger in _loggers:
+    _logger.addHandler(logging.StreamHandler())
+    _logger.setLevel(logging.INFO)
+
+root_logger.setLevel(logging.WARNING)
+
+
+###############################################################################
+# script
+###############################################################################
+
 def main(args: Sequence[str]) -> None:
-    print("Example script.")
-    print(f"Args: {args!s}")
+    start_ts = datetime.now()
+
+    logger.debug("Example script. Args: %s", args)
+
+    try:
+        print("Action: do something")
+    except FileNotFoundError:
+        logger.exception(
+            "Process aborted: a file could not be opened.", exc_info=True)
+    except KeyboardInterrupt:
+        logger.error("Process interrupted by user.")
+    except Exception:
+        logger.exception("Process aborted.")
+    finally:
+        try:
+            print("Action: clean up resources and connections")
+            logger.info("Cleaned up resources and connections.")
+        except Exception:
+            logger.exception("Failed to clean up resources and connections.")
+
+        finish_ts = datetime.now()
+        duration = finish_ts - start_ts
+
+        logger.info(f"start: {start_ts.isoformat()}")
+        logger.info(f"finish: {finish_ts.isoformat()}")
+        logger.info(f"duration: {duration!s}")
 
 
 if __name__ == '__main__':
