93bd7e2d4893c2ebb963c87afcb6c68455bc1cfd
Germán Larraín <glarrain@users.noreply.github.com>
2019-05-31 19:38:19 -0400

    rcv: add module `constants`

--- cl_sii/rcv/__init__.py
+++ cl_sii/rcv/__init__.py
@@ -1,11 +1,12 @@
 """
-SII RCV ("Registro de Compras y Ventas").
+SII RCV ("Registro de Compras y Ventas")
+========================================
 
-.. note::
-    The RCV ("Registro de Compras y Ventas") is composed of 2 "registros":
-    RC ("Registro de Compras") and RV ("Registro de Ventas").
+The RCV ("Registro de Compras y Ventas") is composed of 2 "registros":
+RC ("Registro de Compras") and RV ("Registro de Ventas").
 
 .. seealso::
-    http://www.sii.cl/preguntas_frecuentes/catastro/001_012_6971.htm
+    See SII / FAQ /
+    `¿Qué es el Registro de Compras y Ventas (RCV)? <http://www.sii.cl/preguntas_frecuentes/catastro/001_012_6971.htm>`_  # noqa: E501
 
 """
