52b8cab5e121bbe20b91e4074ea703445d44cdae
Jose Tomas Robles Hahn <jtrh.noreply@jtrh.com>
2019-07-03 18:24:31 -0400

    extras: handle str in Dj RutField.get_prep_value
    The method `cl_sii.extras.dj_model_fields.RutField.get_prep_value(value)`
    expects a `Rut`-typed R.U.T. `value`, which usually is fine.
    
    However, when executing a query, it may be sometimes desirable to pass the
    R.U.T. value as a string rather than as an instance of the `Rut` class.
    
    For example, when using the Django Shell, it is more convenient to type
    `ExampleModel.objects.get(rut='60803000-K')` than
    `ExampleModel.objects.get(rut=Rut('60803000-K'))`, because the former avoids
    needing to import the `Rut` class and is quicker to type. The same thing goes
    for instances of the `uuid.UUID` class.
    
    Before this commit, the `str`-typed R.U.T. would raise an `AttributeError`,
    because `django.db.models.lookups.Lookup.get_prep_lookup()` passes the string to
    `RutField.get_prep_value(value)` without first converting it to a `Rut`
    instance.

--- tests/test_extras_dj_model_fields.py
+++ tests/test_extras_dj_model_fields.py
@@ -8,11 +8,23 @@ from cl_sii.extras.dj_model_fields import Rut, RutField
 class RutFieldTest(unittest.TestCase):
     valid_rut_canonical: str
     valid_rut_instance: Rut
+    valid_rut_verbose_leading_zero_lowercase: str
 
     @classmethod
     def setUpClass(cls) -> None:
         cls.valid_rut_canonical = '60803000-K'
         cls.valid_rut_instance = Rut(cls.valid_rut_canonical)
+        cls.valid_rut_verbose_leading_zero_lowercase = '060.803.000-k'
+
+    def test_get_prep_value_of_canonical_str(self) -> None:
+        prepared_value = RutField().get_prep_value(self.valid_rut_canonical)
+        self.assertIsInstance(prepared_value, str)
+        self.assertEqual(prepared_value, self.valid_rut_canonical)
+
+    def test_get_prep_value_of_non_canonical_str(self) -> None:
+        prepared_value = RutField().get_prep_value(self.valid_rut_verbose_leading_zero_lowercase)
+        self.assertIsInstance(prepared_value, str)
+        self.assertEqual(prepared_value, self.valid_rut_canonical)
 
     def test_get_prep_value_of_Rut(self) -> None:
         prepared_value = RutField().get_prep_value(self.valid_rut_instance)
