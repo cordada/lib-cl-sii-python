97e0d9afb6b380e77209ed4037b28041c8566ba9
Jose Tomas Robles Hahn <jtrh.noreply@jtrh.com>
2019-10-23 15:19:44 -0300

    libs: add module `json_utils`
    Add helper functions (plus a related exception class).
    
    - `read_json_schema`: instantiate an JSON schema dictionary from a file.

---
+++ tests/test_libs_json_utils.py
@@ -0,0 +1,12 @@
+import unittest
+
+from cl_sii.libs.json_utils import read_json_schema  # noqa: F401
+
+from .utils import read_test_file_json_dict  # noqa: F401
+
+
+class FunctionReadJsonSchemaTest(unittest.TestCase):
+
+    # TODO: implement
+
+    pass
