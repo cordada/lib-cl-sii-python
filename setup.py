6c7e731497b6bea5720c9e567e1e29762446e1e4
Germán Larraín <glarrain@users.noreply.github.com>
2020-01-31 13:44:59 -0300

    requirements: update 'lxml'
    Changelog:
    - 4.5.0 (2020-01-29)
      https://github.com/lxml/lxml/blob/lxml-4.5.0/CHANGES.txt#L5
    - 4.4.3 (2020-01-28)
      https://github.com/lxml/lxml/blob/lxml-4.5.0/CHANGES.txt#L31
    - 4.4.2 (2019-11-25)
      https://github.com/lxml/lxml/blob/lxml-4.5.0/CHANGES.txt#L40
    - 4.4.1 (2019-08-11)
      https://github.com/lxml/lxml/blob/lxml-4.5.0/CHANGES.txt#L51
    - 4.4.0 (2019-07-27)
      https://github.com/lxml/lxml/blob/lxml-4.5.0/CHANGES.txt#L63
    - 4.3.5 (2019-07-27)
      https://github.com/lxml/lxml/blob/lxml-4.5.0/CHANGES.txt#L149
    - 4.3.4 (2019-06-10)
      https://github.com/lxml/lxml/blob/lxml-4.5.0/CHANGES.txt#L155
    
    Code diff:
    https://github.com/lxml/lxml/compare/lxml-4.3.3...lxml-4.5.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ requirements = [
     'cryptography>=2.8,<3',
     'defusedxml>=0.5.0,<1',
     'jsonschema>=3.1.1',
-    'lxml>=4.3.3,<5',
+    'lxml>=4.5.0,<5',
     'marshmallow>=2.19.2,<3',
     # TODO: remove upper-bound after a new release of 'signxml' drops the requirement 'pyOpenSSL<19'
     'pyOpenSSL>=18.0.0,<19',
