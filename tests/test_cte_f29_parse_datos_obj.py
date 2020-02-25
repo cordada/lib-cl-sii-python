0c68ee0dbb9b1915d2398da16a00f8afa71e1616
Jose Tomas Robles Hahn <jtrh.noreply@jtrh.com>
2020-02-25 11:52:31 -0300

    cte: Allow four digit Form 29 codes
    Fixes (partially): https://github.com/fyntex/lib-cl-sii-python/issues/93

--- tests/test_cte_f29_parse_datos_obj.py
+++ tests/test_cte_f29_parse_datos_obj.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import datetime
+from decimal import Decimal
 from typing import Any, Mapping
 from unittest import TestCase
 
@@ -112,5 +113,108 @@ class FunctionsTest(TestCase):
             "banco": "Banco del Patito Amarillo",
             "medio_pago": "PEL",
             "fecha_presentacion": datetime.date(2017, 8, 28),
+        }
+        self.assertEqual(dict(obj), expected_dict)
+
+    def test_full_2(self) -> None:
+        datos_obj: Mapping[str, Any] = read_test_file_json_dict(
+            'test_data/sii-cte/f29/cte--6286736-1--f29-6700000056-datos-obj-fake.json',
+        )
+        obj = parse_datos_obj.parse_sii_cte_f29_datos_obj(
+            datos_obj=datos_obj,
+            deserialize_campo=parse_datos_obj.cte_f29_datos_obj_campo_best_effort_deserializer,
+        )
+
+        self.assertIsInstance(obj, data_models.CteForm29)
+        self.assertEqual(obj.contribuyente_rut, Rut('6286736-1'))
+        self.assertEqual(obj.periodo_tributario, PeriodoTributario(year=2018, month=12))
+        self.assertEqual(obj.folio, 6700000056)
+
+        expected_codes_dict = {
+            1: "BONVALLET",
+            2: "GODOY",
+            3: Rut('6286736-1'),
+            5: "EDWARD GUILLERMO",
+            6: "GRECIA 2001",
+            7: 6700000056,
+            8: "MELIPILLA",
+            9: '0',
+            15: PeriodoTributario(year=2018, month=12),
+            48: 22633,
+            55: "E.BONVALLET@EXAMPLE.COM",
+            62: 32875,
+            77: 584976,
+            91: 503286,
+            115: Decimal('0.25'),
+            142: 13000000,
+            151: 447778,
+            315: datetime.date(2019, 1, 22),
+            502: 28500,
+            503: 1,
+            511: 613476,
+            519: 37,
+            520: 613476,
+            521: 2365382,
+            537: 613476,
+            538: 28500,
+            547: 503286,
+            562: 518660,
+            563: 13150000,
+            564: 27,
+            584: 11,
+            586: 1,
+            595: 503286,
+            596: 0,
+            9906: "22/01/2019",
+        }
+        self.assertEqual(obj.as_codes_dict(include_none=False), expected_codes_dict)
+
+        expected_dict = {
+            "contribuyente_rut": Rut('6286736-1'),
+            "periodo_tributario": PeriodoTributario(year=2018, month=12),
+            "folio": 6700000056,
+            "apellido_paterno_o_razon_social": "BONVALLET",
+            "apellido_materno": "GODOY",
+            "nombres": "EDWARD GUILLERMO",
+            "calle_direccion": "GRECIA 2001",
+            "numero_direccion": None,
+            "comuna_direccion": "MELIPILLA",
+            "telefono": "0",
+            "correo_electronico": "E.BONVALLET@EXAMPLE.COM",
+            "representante_legal_rut": None,
+            "extra": {
+                48: 22633,
+                62: 32875,
+                77: 584976,
+                115: Decimal('0.25'),
+                142: 13000000,
+                151: 447778,
+                502: 28500,
+                503: 1,
+                511: 613476,
+                519: 37,
+                520: 613476,
+                521: 2365382,
+                537: 613476,
+                538: 28500,
+                547: 503286,
+                562: 518660,
+                563: 13150000,
+                564: 27,
+                584: 11,
+                586: 1,
+                595: 503286,
+                596: 0,
+                9906: "22/01/2019",
+            },
+            "total_a_pagar_en_plazo_legal": 503286,
+            "total_a_pagar_con_recargo": None,
+            "pct_condonacion": None,
+            "num_res_condonacion": None,
+            "fecha_condonacion": None,
+            "tipo_declaracion": "Rectificatoria con Giro",
+            "banco": "",
+            "medio_pago": "",
+            "fecha_presentacion": datetime.date(2019, 1, 22),
         }
         self.assertEqual(dict(obj), expected_dict)
