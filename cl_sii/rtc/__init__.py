861bc5c8f8a4849f373ee73ef98944188f626231
Jose Tomas Robles Hahn <jtrh.noreply@jtrh.com>
2020-09-10 19:50:51 -0300

    Add sub-package `rtc`
    For SII's "RTC/RPETC".
    
    Concepts and acronyms used interchangeably:
    
    - "Registro Transferencia de Crédito" (RTC)
    - "Registro Público Electrónico de Transferencia de Crédito" (RPETC)
    - "Registro Electrónico de Cesión de Créditos"
    
    This commit was taken from
    https://github.com/fyntex/lib-cl-sii-api-python/commit/50470e85c7eca1dc7f9f3abc4623e26f77f87615.

---
+++ cl_sii/rtc/__init__.py
@@ -0,0 +1,10 @@
+"""
+SII RTC/RPETC
+=============
+
+Concepts and acronyms used interchangeably:
+
+* "Registro Transferencia de Crédito" (RTC)
+* "Registro Público Electrónico de Transferencia de Crédito" (RPETC)
+* "Registro Electrónico de Cesión de Créditos"
+"""
