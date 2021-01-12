0f33beb5815374757916b9eef0b810bac65b1c92
Jose Tomas Robles Hahn <jtrh.noreply@jtrh.com>
2021-01-12 20:37:39 -0300

    rtc.data_models_cesiones_periodo: Create CesionL2 from Ces. Per. Entry
    Add a method to create a `CesionL2` from a `CesionesPeriodoEntry`.

--- tests/test_rtc_data_models_cesiones_periodo.py
+++ tests/test_rtc_data_models_cesiones_periodo.py
@@ -6,7 +6,9 @@ from datetime import date, datetime
 import cl_sii.dte.data_models
 from cl_sii.base.constants import SII_OFFICIAL_TZ
 from cl_sii.dte.constants import TipoDteEnum
+from cl_sii.libs import tz_utils
 from cl_sii.libs.tz_utils import convert_naive_dt_to_tz_aware
+from cl_sii.rtc.data_models import CesionL2
 from cl_sii.rtc.data_models_cesiones_periodo import CesionesPeriodoEntry
 from cl_sii.rut import Rut
 
@@ -115,3 +117,78 @@ class CesionesPeriodoEntryTest(unittest.TestCase):
         self.assertEqual(dte_obj.vendedor_rut, obj.dte_vendedor_rut)
         self.assertEqual(dte_obj.emisor_rut, obj.dte_deudor_rut)
         self.assertEqual(dte_obj.comprador_rut, obj.dte_deudor_rut)
+
+    def test_as_cesion_l2_ok_1(self) -> None:
+        obj = CesionesPeriodoEntry(**self.valid_kwargs)
+        expected_output = CesionL2(
+            dte_key=cl_sii.dte.data_models.DteNaturalKey(
+                emisor_rut=Rut('51532520-4'),
+                tipo_dte=TipoDteEnum.FACTURA_ELECTRONICA,
+                folio=3608460,
+            ),
+            seq=None,
+            cedente_rut=Rut('51532520-4'),
+            cesionario_rut=Rut('96667560-8'),
+            fecha_cesion_dt=tz_utils.convert_naive_dt_to_tz_aware(
+                dt=datetime(2019, 3, 7, 13, 32),
+                tz=CesionL2.DATETIME_FIELDS_TZ,
+            ),
+            monto_cedido=256357,
+            dte_receptor_rut=Rut('75320502-0'),
+            dte_fecha_emision=date(2019, 2, 11),
+            dte_monto_total=256357,
+            fecha_ultimo_vencimiento=date(2019, 4, 12),
+            cedente_razon_social='MI CAMPITO SA',
+            cedente_email='mi@campito.cl',
+            cesionario_razon_social='POBRES SERVICIOS FINANCIEROS S.A.',
+            cesionario_email='un-poco@pobres.cl,super.ejecutivo@pobres.cl',
+        )
+        obj_cesion_l2 = obj.as_cesion_l2()
+        self.assertEqual(obj_cesion_l2, expected_output)
+
+        self.assertIsNone(obj_cesion_l2.natural_key)
+        self.assertEqual(obj_cesion_l2.alt_natural_key.dte_key.emisor_rut, obj.dte_vendedor_rut)
+        self.assertEqual(obj_cesion_l2.alt_natural_key.cedente_rut, obj.cedente_rut)
+        self.assertEqual(obj_cesion_l2.alt_natural_key.cesionario_rut, obj.cesionario_rut)
+        self.assertEqual(obj_cesion_l2.alt_natural_key.fecha_cesion_dt, obj.fecha_cesion_dt)
+
+        self.assertEqual(obj_cesion_l2.dte_receptor_rut, obj.dte_deudor_rut)
+
+    def test_as_cesion_l2_ok_2(self) -> None:
+        self.valid_kwargs.update(dict(
+            dte_tipo_dte=TipoDteEnum.FACTURA_COMPRA_ELECTRONICA,
+        ))
+        obj = CesionesPeriodoEntry(**self.valid_kwargs)
+        expected_output = CesionL2(
+            dte_key=cl_sii.dte.data_models.DteNaturalKey(
+                emisor_rut=Rut('75320502-0'),
+                tipo_dte=TipoDteEnum.FACTURA_COMPRA_ELECTRONICA,
+                folio=3608460,
+            ),
+            seq=None,
+            cedente_rut=Rut('51532520-4'),
+            cesionario_rut=Rut('96667560-8'),
+            fecha_cesion_dt=tz_utils.convert_naive_dt_to_tz_aware(
+                dt=datetime(2019, 3, 7, 13, 32),
+                tz=CesionL2.DATETIME_FIELDS_TZ,
+            ),
+            monto_cedido=256357,
+            dte_receptor_rut=Rut('51532520-4'),
+            dte_fecha_emision=date(2019, 2, 11),
+            dte_monto_total=256357,
+            fecha_ultimo_vencimiento=date(2019, 4, 12),
+            cedente_razon_social='MI CAMPITO SA',
+            cedente_email='mi@campito.cl',
+            cesionario_razon_social='POBRES SERVICIOS FINANCIEROS S.A.',
+            cesionario_email='un-poco@pobres.cl,super.ejecutivo@pobres.cl',
+        )
+        obj_cesion_l2 = obj.as_cesion_l2()
+        self.assertEqual(obj_cesion_l2, expected_output)
+
+        self.assertIsNone(obj_cesion_l2.natural_key)
+        self.assertEqual(obj_cesion_l2.alt_natural_key.dte_key.emisor_rut, obj.dte_deudor_rut)
+        self.assertEqual(obj_cesion_l2.alt_natural_key.cedente_rut, obj.cedente_rut)
+        self.assertEqual(obj_cesion_l2.alt_natural_key.cesionario_rut, obj.cesionario_rut)
+        self.assertEqual(obj_cesion_l2.alt_natural_key.fecha_cesion_dt, obj.fecha_cesion_dt)
+
+        self.assertEqual(obj_cesion_l2.dte_receptor_rut, obj.dte_vendedor_rut)
