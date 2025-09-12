6438d2a493fe52f7aa855ba507594ec6e2d22b6a
Samuel Villegas <svillegas@cordada.com>
2025-09-12 12:49:49 -0300

    feat(cte): Add parser for "Datos del Contribuyente"
    - Implemented `parse_taxpayer_data` to parse taxpayer data from CTE HTML.
    - Added `TaxpayerData` and `LastFiledDocument` data models.
    - Created tests to validate parser functionality with sample HTML input.
    
    Ref: https://app.shortcut.com/cordada/story/16534/

--- src/tests/test_cte_parsers.py
+++ src/tests/test_cte_parsers.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from datetime import date
 from unittest import TestCase
 
 from cl_sii.cte import data_models, parsers
@@ -49,3 +50,58 @@ class ParsersTest(TestCase):
                 assert_raises_cm.exception.args,
                 ("Could not find taxpayer information table in HTML",),
             )
+
+    def test_parse_taxpayer_data(self) -> None:
+        html_content = read_test_file_str_utf8('test_data/sii-cte/cte_empty_f29.html')
+        with self.subTest("Parsing ok"):
+            result = parsers.parse_taxpayer_data(html_content)
+            expected_obj = data_models.TaxpayerData(
+                start_of_activities_date=date(2023, 11, 15),
+                economic_activities=(
+                    "SERVICIOS DE ASESORIA Y CONSULTORIA EN MATERIA DE ADMINISTRACION DE EMPRESAS "
+                    "Y OTROS SERVICIOS DE ASESORIA ADMINISTRATIVA Y DE NEGOCIOS N.C.P.\n"
+                    "ACTIVIDADES DE OTRAS ORGANIZACIONES EMPRESARIALES N.C.P.\n"
+                    "OTRAS ACTIVIDADES DE SERVICIOS PERSONALES N.C.P."
+                ),
+                tax_category="Primera categoría",
+                address="AV REAL, LAS CONDES",
+                branches=[],
+                last_filed_documents=[
+                    data_models.LastFiledDocument(
+                        name="FACTURA ELECTRONICA", date=date(2025, 7, 24)
+                    ),
+                    data_models.LastFiledDocument(
+                        name="FACTURA NO AFECTA O EXENTA ELECTRONICA", date=date(2025, 7, 17)
+                    ),
+                    data_models.LastFiledDocument(
+                        name="GUIA DESPACHO ELECTRONICA", date=date(2025, 5, 14)
+                    ),
+                    data_models.LastFiledDocument(
+                        name="NOTA CREDITO ELECTRONICA", date=date(2025, 7, 18)
+                    ),
+                ],
+                tax_observations="No tiene observaciones.",
+            )
+            self.assertEqual(result, expected_obj)
+
+        with self.subTest("Parsing empty content"):
+            with self.assertRaises(ValueError) as assert_raises_cm:
+                parsers.parse_taxpayer_data("")
+            self.assertEqual(
+                assert_raises_cm.exception.args,
+                ("Could not find 'Datos del Contribuyente' table in HTML",),
+            )
+
+        with self.subTest("Parsing content with empty table"):
+            html_content = read_test_file_str_utf8('test_data/sii-cte/cte_empty_table.html')
+            result = parsers.parse_taxpayer_data(html_content)
+            expected_obj = data_models.TaxpayerData(
+                start_of_activities_date=None,
+                economic_activities="",
+                tax_category="",
+                address="",
+                branches=[],
+                last_filed_documents=[],
+                tax_observations=None,
+            )
+            self.assertEqual(result, expected_obj)
